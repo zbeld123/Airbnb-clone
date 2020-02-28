842f74d52c66a713c290d9b472f7dd07f7d566fa
전혜지 <jeonhyeji@jeonhyejiui-MacBookAir.local>
2020-02-28 12:58:12 +0900

    room model Definition (..ing)

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-
-
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class RoomTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ """
+
     pass
