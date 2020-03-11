fec96778f2e61571a98c73c25d0fa8d56f5c9557
전혜지 <jeonhyeji@jeonhyejiui-MacBookAir.local>
2020-03-11 14:27:48 +0900

    Design homeView | detail : Design pnpagenation by pure python coding style

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,26 @@
+from math import ceil
+from . import models
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page * page_size
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
